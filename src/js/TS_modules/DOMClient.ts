d30015bf1a6035e154be36c0c9591b90d80d589c
Kirill Chernov <alexpivch@gmail.com>
2021-03-24 06:59:35 +0200

    Added a basic DOM logic

---
+++ src/js/TS_modules/DOMClient.ts
@@ -0,0 +1,104 @@
+export class ElementClient {
+  public calendarElment: HTMLElement;
+  public inputElement: HTMLInputElement;
+  public form: HTMLFormElement;
+  public listWrapper: HTMLTableSectionElement;
+  public currentDate: Date;
+  public currentCalendarPoint: HTMLElement;
+  public datePerform: HTMLSpanElement;
+
+  constructor() {
+    this.calendarElment = document.querySelector("#myCalendar");
+    this.inputElement = document.querySelector("#ctrlText");
+    this.form = document.querySelector("#ctrlForm");
+    this.listWrapper = document.querySelector(".list");
+    this.datePerform = document.querySelector("#ctrlDate");
+  }
+
+  setSelectedElements(date: Date, calendarPoint: HTMLElement) {
+    this.currentDate = date;
+    this.currentCalendarPoint = calendarPoint;
+  }
+
+  set calendarBody(element: HTMLElement) {
+    this.calendarBody = this.calendarBody;
+  }
+}
+
+export class EventClient extends ElementClient {
+  protected _currentDate: Date;
+  protected _currentCalendarPoint: HTMLElement;
+
+  constructor(
+    public storageControllerClass: any,
+    public renderClass: any,
+    public messageController: any,
+    public domStorage: any,
+    public idGenerator: any
+  ) {
+    super();
+    this.storageControllerClass = storageControllerClass;
+    this.renderClass = renderClass;
+    this.messageController = messageController;
+    (this.domStorage = domStorage), (this.idGenerator = idGenerator);
+  }
+
+  initFormEvents(): void {
+    this.form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      if (!this.domStorage.inputElement.value) {
+        this.messageController.show("You need to enter correctly value!");
+        return;
+      }
+
+      if (this.domStorage.datePerform.innerText == "Chose the date pls!") {
+        this.messageController.show("You need to chose a date!");
+        return;
+      }
+      this.storageControllerClass.saveTask({
+        taskContent: this.domStorage.inputElement.value,
+        isDone: false,
+        randomID: this.idGenerator.generate(),
+        taskDay: (+this.domStorage.currentDate).toString(),
+      });
+
+      this.renderClass.render();
+    });
+  }
+
+  initListEvents(): void {
+    this.listWrapper.addEventListener("click", (event) => {
+      let target: any = event.target;
+      if (target.matches(".list__button")) {
+        this.storageControllerClass.delete(target.parentElement.dataset.taskId);
+        this.renderClass.render();
+      }
+      if (target.matches(".list__item")) {
+        target.classList.toggle("list__item_checked");
+        if (target.classList.contains("list__item_checked")) {
+          let newOptions: any = this.storageControllerClass.takeTaskById(
+            target.dataset.taskId
+          );
+          newOptions.isDone = true;
+          this.storageControllerClass.updateTask(newOptions);
+        } else {
+          let newOptions: any = this.storageControllerClass.takeTaskById(
+            target.dataset.taskId
+          );
+          newOptions.isDone = false;
+          this.storageControllerClass.updateTask(newOptions);
+        }
+      }
+    });
+  }
+
+  initCalendarChangeEvents(): void {
+    let buttons: any = this.calendarElment.querySelectorAll("button");
+    for (let button of buttons) {
+      button.addEventListener("click", () => {
+        this.renderClass.render();
+      });
+    }
+  }
+}
--- src/js/function.ts
+++ src/js/function.ts
@@ -1,7 +1,10 @@
 declare var VanillaCalendar;
 
 import { ListComponent } from "./TS_modules/components";
-
 import { Storage, StorageController } from "./TS_modules/storage";
-
 import { IdGenerator } from "./TS_modules/idGenerator";
+import { MessageClient } from "./TS_modules/messageClient";
+import { Render } from "./TS_modules/render";
+import { ElementClient, EventClient } from './TS_modules/DOMClient';
+
+
