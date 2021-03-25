3e574f9be501b69021d62d843367a84d5cda2845
Kirill Chernov <alexpivch@gmail.com>
2021-03-25 03:09:50 +0200

    Added the favicons and manifest file

---
+++ docs/dist/manifest.dev.js
@@ -0,0 +1,84 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+parcelRequire = function (e, r, t, n) {
+  var i,
+      o = "function" == typeof parcelRequire && parcelRequire,
+      u = "function" == typeof require && require;
+
+  function f(t, n) {
+    if (!r[t]) {
+      if (!e[t]) {
+        var i = "function" == typeof parcelRequire && parcelRequire;
+        if (!n && i) return i(t, !0);
+        if (o) return o(t, !0);
+        if (u && "string" == typeof t) return u(t);
+        var c = new Error("Cannot find module '" + t + "'");
+        throw c.code = "MODULE_NOT_FOUND", c;
+      }
+
+      p.resolve = function (r) {
+        return e[t][1][r] || r;
+      }, p.cache = {};
+      var l = r[t] = new f.Module(t);
+      e[t][0].call(l.exports, p, l, l.exports, this);
+    }
+
+    return r[t].exports;
+
+    function p(e) {
+      return f(p.resolve(e));
+    }
+  }
+
+  f.isParcelRequire = !0, f.Module = function (e) {
+    this.id = e, this.bundle = f, this.exports = {};
+  }, f.modules = e, f.cache = r, f.parent = o, f.register = function (r, t) {
+    e[r] = [function (e, r) {
+      r.exports = t;
+    }, {}];
+  };
+
+  for (var c = 0; c < t.length; c++) {
+    try {
+      f(t[c]);
+    } catch (e) {
+      i || (i = e);
+    }
+  }
+
+  if (t.length) {
+    var l = f(t[t.length - 1]);
+    "object" == (typeof exports === "undefined" ? "undefined" : _typeof(exports)) && "undefined" != typeof module ? module.exports = l : "function" == typeof define && define.amd ? define(function () {
+      return l;
+    }) : n && (this[n] = l);
+  }
+
+  if (parcelRequire = f, i) throw i;
+  return f;
+}({
+  "Sx6z": [function (require, module, exports) {
+    module.exports = {
+      lang: "en",
+      name: "ToDoApp",
+      short_name: "TDA",
+      icons: [{
+        src: "src/img/favicons/64.png",
+        sizes: "64x64",
+        type: "image/png"
+      }, {
+        src: "src/img/favicons/64.png",
+        sizes: "64x64"
+      }, {
+        src: "src/img/favicons/128.png",
+        sizes: "128x128",
+        density: 2
+      }],
+      scope: "/",
+      start_url: "/index.htm",
+      display: "fullscreen",
+      orientation: "portrait"
+    };
+  }, {}]
+}, {}, ["Sx6z"], null);
